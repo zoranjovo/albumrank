9aa9b81199cedf71331f68894174cc3703b5507f
zoranjovo <50138575+zoranjovo@users.noreply.github.com>
2025-08-31 01:00:57 +1000

    Update cache.ts

--- app/api/cache.ts
+++ app/api/cache.ts
@@ -1,7 +1,7 @@
 import Database from 'better-sqlite3';
 import path from 'path';
 
-const db = new Database(path.join(process.cwd(), 'tmp', 'cache.db'));
+const db = new Database(path.join('./tmp', 'cache.db'));
 db.pragma('journal_mode = WAL');
 
 // initalise table
